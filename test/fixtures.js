eb171b6cee501bb2e3675119e16d3cc9ecd418bb
Henry Darana <henry.darana@tray.com>
2024-06-05 18:00:33 +0530

    Sesssion | Testing Get Calls

--- test/fixtures.js
+++ test/fixtures.js
@@ -2,10 +2,42 @@ import supertest from "supertest";
 import { expect } from "chai";
 const request = supertest("https://fantasy.premierleague.com/api/");
 
-describe("Fixtures", () => {
+describe("Fantasy Premier League", () => {
   it("GET /fixtures", () => {
-    request.get("fixtures/").end((err, res) => {
+    return request.get("fixtures/").then((res) => {
       expect(res.body).to.not.be.empty;
+    });
+  });
+
+  it("GET element-summary/:id/", () => {
+    return request.get("element-summary/1/").then((res) => {
+      expect(res.body.history).to.not.be.empty;
+    });
+  });
+
+  it("GET bootstrap-static/", () => {
+    return request.get("bootstrap-static/").then((res) => {
+      expect(res.body).to.have.property("events");
+      expect(res.body).to.have.property("teams");
+      expect(res.body).to.have.property("element_types");
+    });
+  });
+
+  it("GET bootstrap-static/teams-size", () => {
+    return request.get("bootstrap-static/").then((res) => {
+      expect(res.body.teams).to.have.length(20);
+    });
+  });
+
+  it("GET bootstrap-static/total_players", () => {
+    return request.get("bootstrap-static/").then((res) => {
+      expect(res.body.total_players).to.be.eq(10905021);
+    });
+  });
+
+  it("GET bootstrap-static/elements", () => {
+    return request.get("bootstrap-static/").then((res) => {
+      expect(res.body.elements[0].first_name).to.be.eq("Folarin");
     });
   });
 });
--- test/users.js
+++ test/users.js
@@ -1,12 +1,38 @@
 import supertest from "supertest";
 import { expect } from "chai";
-const request = supertest("https://gorest.co.in/public-api/");
+const request = supertest("https://gorest.co.in/public/v2/");
 const TOKEN = "";
 
 describe("Users", () => {
+  it("GET /users", (done) => {
+    request.get(`users?access-token=${TOKEN}`).end((err, res) => {
+      expect(res.body).to.not.be.empty;
+      done();
+    });
+  });
+});
+
+describe("Users#", () => {
   it("GET /users", () => {
-    request.get("users").end((err, res) => {
-      expect(res.body.data).to.not.be.empty;
+    return request.get(`users?access-token=${TOKEN}`).then((res) => {
+      expect(res.body).to.not.be.empty;
+    });
+  });
+
+  it("GET /users/:id", () => {
+    return request.get(`users/6947823?access-token=${TOKEN}`).then((res) => {
+      expect(res.body.id).to.be.eq(6947823);
+    });
+  });
+
+  it("GET /users with params", () => {
+    const endpoint = `users/?access-token=${TOKEN}&page=7&gender=male&status=active`;
+    return request.get(endpoint).then((res) => {
+      expect(res.body).to.not.be.empty;
+      res.body.forEach((element) => {
+        expect(element.gender).to.be.eq("male");
+        expect(element.status).to.be.eq("active");
+      });
     });
   });
 });
